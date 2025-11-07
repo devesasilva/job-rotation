b92273867aea4844f655fe20aefd6755c20a2ffd
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2025-11-07 02:41:16 +0000

    Centralizado form de login

--- frontend/job-rotation/src/pages/Login.jsx
+++ frontend/job-rotation/src/pages/Login.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LoginCard from '../components/auth/LoginCard';
 
 function Login() {
   return (
-    <div className="min-h-screen flex items-start justify-center bg-white relative overflow-hidden">
-      <div aria-hidden className="absolute -top-24 -left-24 w-[420px] h-[420px] rounded-full bg-purple-300 opacity-80"></div>
-      <div aria-hidden className="absolute -top-40 right-0 w-[360px] h-[360px] rounded-full bg-green-200 opacity-90"></div>
-      <div aria-hidden className="absolute top-8 left-1/2 -translate-x-1/2 w-[300px] h-[300px] rounded-full bg-pink-300 opacity-90"></div>
+    <div className="h-screen flex items-center justify-center bg-white relative overflow-hidden">
+      <div
+        aria-hidden
+        className="absolute -top-24 -left-24 w-[420px] h-[420px] rounded-full bg-red-300 opacity-80 blur-3xl"
+      ></div>
 
-      <div className="w-full max-w-md mt-28 px-4">
-        <LoginCard />
-      </div>
+      <div
+        aria-hidden
+        className="absolute -top-40 right-0 w-[360px] h-[360px] rounded-full bg-lime-300 opacity-80 blur-2xl"
+      ></div>
+
+      <div
+        aria-hidden
+        className="absolute top-8 left-1/2 -translate-x-1/2 w-[300px] h-[300px] rounded-full bg-violet-500 opacity-70 blur-2xl"
+      ></div>
+
+      <div
+        aria-hidden
+        className="absolute bottom-0 left-1/4 w-[260px] h-[260px] rounded-full bg-fuchsia-400 opacity-60 blur-2xl"
+      ></div>
+
+        <div className="w-full max-w-md px-4 z-10">
+          <LoginCard />
+        </div>
     </div>
   );
 }
