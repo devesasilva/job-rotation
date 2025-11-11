e634efda52c8f7c0bb175ce1e35923ad076f7b35
melissaromao <melissadiascntt@gmail.com>
2025-11-10 22:01:12 -0300

    Adicionando rota principal home

--- frontend/job-rotation/src/App.jsx
+++ frontend/job-rotation/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import './index.css';
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
