458719cdb5cbb8fbe442d675844fd397771053cc
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2025-11-29 10:53:23 -0300

    Permite adicionar pessoas sem cadastro no rodízio.

--- backend/src/models/Rodizio.js
+++ backend/src/models/Rodizio.js
@@ -1,58 +1,56 @@
 const mongoose = require('mongoose');
 
 const rodizioSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        required: true,
-    },
-    descricao: {
-        type: String
-    },
-    ciclo: {
-        type: String,
-        enum: ['Diário', 'Semanal', 'Quinzenal', 'Mensal', 'Anual'],
-        required: true,
-    },
-    setor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Setor',
-        required: true,
-    },
-    membros: [{
-        usuario: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Usuario',
-            required: true,
-        },
-        funcao: String,
-    }],
-    necessidades: [
-        {
-            habilidade: {
-                type: String,
-                required: true,
-            },
-            formacao: {
-                type: String,
-                required: true,
-            },
-            quantidade: {
-                type: Number,
-                required: true,
-            }
-        }
-    ],
-    dataInicio: {
-        type: Date,
-        required: true,
-    },
-    dataFim: {
-        type: Date,
-        required: true,
-    },
+    nome: {
+        type: String,
+        required: true,
+    },
+    descricao: {
+        type: String
+    },
+    ciclo: {
+        type: String,
+        enum: ['Diário', 'Semanal', 'Quinzenal', 'Mensal', 'Anual'],
+        required: true,
+    },
+    setor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Setor',
+        required: true,
+    },
+    membros: [{
+        nome: { 
+            type: String,
+            required: true,
+        },
+        funcao: String, 
+    }],
+    necessidades: [
+        {
+            habilidade: {
+                type: String,
+                required: true,
+            },
+            formacao: {
+                type: String,
+                required: true,
+            },
+            quantidade: {
+                type: Number,
+                required: true,
+            }
+        }
+    ],
+    dataInicio: {
+        type: Date,
+        required: true,
+    },
+    dataFim: {
+        type: Date,
+        required: true,
+    },
 }, { timestamps: true });
 
 const Rodizio = mongoose.models.Rodizio || mongoose.model('Rodizio', rodizioSchema);
-
 
 module.exports = Rodizio;
