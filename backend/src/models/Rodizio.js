4461649fc76c2534a05538ef56c5377b298430f6
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2025-11-29 11:46:40 -0300

    fix: membros

--- backend/src/models/Rodizio.js
+++ backend/src/models/Rodizio.js
@@ -1,54 +1,45 @@
 const mongoose = require('mongoose');
 
 const rodizioSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        required: true,
-    },
-    descricao: {
-        type: String
-    },
-    ciclo: {
-        type: String,
-        enum: ['Diário', 'Semanal', 'Quinzenal', 'Mensal', 'Anual'],
-        required: true,
-    },
-    setor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Setor',
-        required: true,
-    },
-    membros: [{
-        nome: { 
-            type: String,
-            required: true,
-        },
-        funcao: String, 
-    }],
-    necessidades: [
-        {
-            habilidade: {
-                type: String,
-                required: true,
-            },
-            formacao: {
-                type: String,
-                required: true,
-            },
-            quantidade: {
-                type: Number,
-                required: true,
-            }
-        }
-    ],
-    dataInicio: {
-        type: Date,
-        required: true,
-    },
-    dataFim: {
-        type: Date,
-        required: true,
-    },
+    nome: {
+        type: String,
+        required: true,
+    },
+    descricao: {
+        type: String
+    },
+    ciclo: {
+        type: String,
+        enum: ['Diário', 'Semanal', 'Quinzenal', 'Mensal', 'Anual'],
+    },
+    setor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Setor',
+    },
+    
+    membros: [{
+        type: String,
+        required: true,
+    }],
+    
+    funcoes: [{
+        type: String,
+        required: true,
+    }],
+    
+    dataInicio: {
+        type: Date,
+        required: true,
+    },
+    dataFim: {
+        type: Date,
+        required: true,
+    },
+    duracaoTurno: {
+        type: Number,
+        required: true,
+    }
+
 }, { timestamps: true });
 
 const Rodizio = mongoose.models.Rodizio || mongoose.model('Rodizio', rodizioSchema);
