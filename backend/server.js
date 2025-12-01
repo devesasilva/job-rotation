b1092d7855f532d972069db199f7931ce16df093
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2025-12-01 23:38:01 +0000

    Testado versão em estado good da aplicação

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const ConexaoDB = require("./config/db");
 const {swaggerUi, swaggerSpec} = require('./config/swagger');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 ConexaoDB();
 
