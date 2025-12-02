6e33a2e5871905497635e736a19e4f52dc6de51b
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2025-12-02 20:51:26 -0300

    Teste de isolamento Swagger

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const ConexaoDB = require("./config/db");
-const { swaggerUi, swaggerSpec } = require("./config/swagger");
+//const { swaggerUi, swaggerSpec } = require("./config/swagger");
 
 const PORT = process.env.PORT || 3000;
 
@@ -30,7 +30,7 @@ app.use(express.json());
 
 app.use("/", require("./src/routes/index"));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
