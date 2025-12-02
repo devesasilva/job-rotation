2d2836dd68c221973153127f8a5f99a630fb8b00
Laine <devesadasilvalaine@gmail.com>
2025-12-02 13:15:50 -0300

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 ConexaoDB();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ 
+  origin: ['http://localhost:5173', 
+            'https://job-rotation.vercel.app'
+  ],
+  credentials: true }));
 app.use(express.json()); 
 
 app.use("/", require("./src/routes/index")); 
