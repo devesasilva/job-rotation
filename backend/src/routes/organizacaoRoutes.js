38838f0336dc39568c67cd21add6cb50ed8218ee
melissaromao <melissadiascntt@gmail.com>
2026-09-12 00:00:20 -0300

    🗺️ feat: adiciona mapeamento de rotas para membros em organizacoes

--- backend/src/routes/organizacaoRoutes.js
+++ backend/src/routes/organizacaoRoutes.js
@@ -10,4 +10,9 @@ router.use(authMiddleware);
 router.post('/criar', organizacaoController.criarOrganizacao);
 router.get('/:id', organizacaoController.buscarOrganizacao);
 
+router.post('/:id/membros', organizacaoController.adicionarMembro);
+router.get('/:id/membros', organizacaoController.listarMembros);
+router.put('/:id/membros/:membroId', organizacaoController.editarMembro);
+router.delete('/:id/membros/:membroId', organizacaoController.removerMembro);
+
 module.exports = router;
